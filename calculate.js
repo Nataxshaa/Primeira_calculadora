1d3c896de722543eb3c59ff344f058a5a52a78d8
Leonardo-LC <leonardolopescrz@gmail.com>
2024-09-04 12:49:08 -0300

    In addition to some visual changes, I added a Light and Dark mode using some JavaScript

--- calculate.js
+++ calculate.js
@@ -40,3 +40,33 @@ function calculate() {
         document.getElementById('display').value = 'error';
     }
 }
+
+
+
+function theme() {
+    const checkbox = document.getElementById('checkbox'); 
+    const corpo = document.querySelector('body');
+    const calculadora = document.querySelector('.titulo');
+    const botoes = document.querySelectorAll('.botao');
+
+    if (checkbox.checked) {
+        corpo.setAttribute('class', 'dark');
+        calculadora.classList.add('tLight');
+        calculadora.classList.remove('tDark');
+        botoes.forEach(botao => {
+            botao.classList.add('bDark');
+            botao.classList.remove('bLight');
+        });
+    } else {
+        corpo.setAttribute('class', 'light');
+        calculadora.classList.add('tDark');
+        calculadora.classList.remove('tLight');
+        botoes.forEach(botao => {
+            botao.classList.add('bLight');
+            botao.classList.remove('bDark');
+        });
+    }
+}
+
+
+
